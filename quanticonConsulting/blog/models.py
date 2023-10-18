e6012f9f71fd8ae4a7f7aac06681c8a118cad144
jlbalde2 <89554904+jlbalde2@users.noreply.github.com>
2023-10-17 19:14:06 -0500

    blog summary and post detail pages

--- quanticonConsulting/blog/models.py
+++ quanticonConsulting/blog/models.py
@@ -1,3 +1,60 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+STATUS = ((0, "Draft"), (1, "Publish"))
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to="images/", default="images/default.png")
+    image_two = models.ImageField(upload_to="images/", default="images/default.png")
+    image_three = models.ImageField(upload_to="images/", default="images/default.png")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+        verbose_name = _("Post")
+        verbose_name_plural = _("Posts")
+
+    def get_absolute_url(self):
+        return reverse("blog:post_details", args=[self.slug])
+
+    def __str__(self):
+        return self.title
+
+
+class BlogImage(models.Model):
+    """
+    The Product Image table.
+    """
+
+    product = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="blog_image")
+    image = models.ImageField(
+        verbose_name=_("image"),
+        help_text=_("Upload a product image"),
+        upload_to="images/",
+        default="media/images/default.png",
+    )
+    alt_text = models.CharField(
+        verbose_name=_("Alternative text"),
+        help_text=_("Please add alternative text"),
+        max_length=255,
+        null=True,
+        blank=True,
+    )
+    is_feature = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("Product Image")
+        verbose_name_plural = _("Product Images")
--- quanticonConsulting/blog/urls.py
+++ quanticonConsulting/blog/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
 
-app_name = "blog" 
-urlpatterns = [
+app_name = "blog"
 
-]
+urlpatterns = [
+    path("blogsummary/", views.PostList.as_view(), name="home"),
+    path("<slug:slug>/", views.PostDetail.as_view(), name="post_detail"),
+] 
