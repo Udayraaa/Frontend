c913f123648c9d2ecae05de776ec8ceb1ab14995
Sc0rpinok <112124620+Sc0rpinok@users.noreply.github.com>
2023-10-27 23:17:01 -0500

    Contact Form DB Connection
    Contact Form now inserts into SQL table and prints form to terminal. To have it actually send an email, it requires an SMTP server and setup

--- quanticonConsulting/website/views.py
+++ quanticonConsulting/website/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 from django.db import connection
 
+from django.db import IntegrityError
+
 from django.shortcuts import render, redirect
 from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
@@ -53,8 +55,23 @@ def contact(request):
 			}
 			message = "\n".join(body.values())
 
+			# Defining table values
+			form_first_name = form.cleaned_data['first_name']
+			form_last_name = form.cleaned_data['last_name']
+			form_email = form.cleaned_data['email_address']
+			form_message = form.cleaned_data['message']
+			form_subject = "Website Inquiry"
+
 			try:
-				send_mail(subject, message, 'admin@example.com', ['admin@example.com']) # subject = From messsage = To
+                # Send the email
+				send_mail(subject, message, form_email, ['thisisnotaturtle@gmail.com']) # subject = From messsage = To
+
+				with connection.cursor() as cursor:
+					# Insert data to database table
+					sql_insert = "INSERT INTO dbo.ContactSubmissions (name, email, subject, message, date) VALUES (%s, %s, %s, %s, GETDATE())"
+					cursor.execute(sql_insert, (f"{form_first_name} {form_last_name}", form_email, form_subject, form_message))
+                
+
 			except BadHeaderError:
 				return HttpResponse('Invalid header found.')
 			return redirect ("/")
